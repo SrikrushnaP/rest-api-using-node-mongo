7578ee0208f5eecf653348661ef8065328a671b5
Srikrushna <palsrikrushna6@gmail.com>
2023-05-06 21:32:17 +0530

    Added error handeling on serve.js file

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require("express");
+const http = require("http");
 
-const app = express(); 
+const app = express();
+const port = 3200;
 
-app.get('/', function(req, res){
+const onError = error => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+app.get('/', function (req, res) {
     res.send("Hello from server");
 });
 
-app.listen(3200, function(){
-    console.log("Server  is running on localhost: 3200");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.listen(port, function () {
+    console.log("Server is running on localhost:", port);
 })
